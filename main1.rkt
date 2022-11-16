c6e3e858da71cab629b210690f8766f0f81e3fe8
CUWSulli <78753434+CUWSulli@users.noreply.github.com>
2022-11-16 11:27:24 -0600

    Add files via upload

---
+++ main1.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(require "utility.rkt")
+(require "runner.rkt")
+(require "parcer.rkt")
+(require "variable_env.rkt")
+
+(define env '((global (a 1) (b 2) (c 5))))
+
+(define sample-code '(block (assign x 8) (print x))) ;SCREEN 1
+(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
+(displayln (neo-parser sample-code))
+(define parsed-neo-code (neo-parser sample-code))
+(run-neo-parsed-code parsed-neo-code env)
+
